daed649cb9d84312f9703e275e94ebed13394325
Administrator <3537156682@qq.com>
2020-08-29 09:15:58 +0800

    提交usercontroller

--- src/main/java/com/hy/crm/controller/UserroleController.java
+++ src/main/java/com/hy/crm/controller/UserroleController.java
@@ -17,7 +17,8 @@ import org.springframework.stereotype.Controller;
 @RequestMapping("//userrole")
 public class UserroleController {
     public static void main(String[] args){
-    System.out.println("第一次提交");
+        System.out.println("第一次提交");
+        System.out.println("第二次提交");
     }
 
 }
