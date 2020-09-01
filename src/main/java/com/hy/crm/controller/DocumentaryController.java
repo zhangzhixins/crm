902cfd8f4bcb0d65542f7615d76f97823e5c4698
yangsen <yangs251@qq.com>
2020-09-01 18:00:01 +0800

    UserroleController杨森第一次提交

--- src/main/java/com/hy/crm/controller/DocumentaryController.java
+++ src/main/java/com/hy/crm/controller/DocumentaryController.java
@@ -1,6 +1,10 @@
 package com.hy.crm.controller;
 
 
+import com.hy.crm.pojo.Documentary;
+import com.hy.crm.service.IDocumentaryService;
+import com.hy.crm.service.impl.DocumentaryServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
@@ -14,7 +18,15 @@ import org.springframework.stereotype.Controller;
  * @since 2020-08-28
  */
 @Controller
-@RequestMapping("//documentary")
+@RequestMapping("/documentary")
 public class DocumentaryController {
+    @Autowired
+    private IDocumentaryService iDocumentaryService;
+
+    @RequestMapping("/adddoc.do")
+    public void adddocumentary(Documentary documentary){
+        System.out.println(documentary.toString());
+        iDocumentaryService.save(documentary);
+    }
 
 }
