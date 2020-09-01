9fc325493e9d8c030e3d71a493c4b70b80695473
zzx <651093386@qq.com>
2020-09-01 09:22:02 +0800

    utis包下面的msg上传

---
+++ src/main/java/com/hy/crm/utils/MsgUtils.java
@@ -0,0 +1,40 @@
+package com.hy.crm.utils;
+
+public class MsgUtils {
+    private String code;
+    private String msg;
+    private Integer count;
+    private Object data;
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setData(Object data) {
+        this.data = data;
+    }
+}
