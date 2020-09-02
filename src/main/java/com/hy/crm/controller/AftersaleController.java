765b615d4d557070f624864511ede899d3367349
zzx <651093386@qq.com>
2020-09-02 10:49:36 +0800

    售后服务列表和新增页面

--- src/main/java/com/hy/crm/controller/AftersaleController.java
+++ src/main/java/com/hy/crm/controller/AftersaleController.java
@@ -1,9 +1,18 @@
 package com.hy.crm.controller;
 
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.hy.crm.pojo.vo.AftersaleExt;
+import com.hy.crm.service.IAftersaleService;
+import com.hy.crm.utils.MsgUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 /**
  * <p>
@@ -14,7 +23,21 @@ import org.springframework.stereotype.Controller;
  * @since 2020-08-28
  */
 @Controller
-@RequestMapping("//aftersale")
+@RequestMapping("/aftersale")
 public class AftersaleController {
 
+    @Autowired
+    IAftersaleService aftersaleService;
+
+    @RequestMapping("/queryAll.do")
+    @ResponseBody
+    public MsgUtils queryAll(String val,String keyWord,Integer page,Integer limit){
+        IPage<AftersaleExt> iPage=aftersaleService.queryAll(val,keyWord,page,limit);
+        MsgUtils msgUtils = new MsgUtils();
+        msgUtils.setCode("0");
+        msgUtils.setMsg("查询成功");
+        msgUtils.setCount(Integer.parseInt(String.valueOf(iPage.getTotal())));
+        msgUtils.setData(iPage.getRecords());
+        return msgUtils;
+    }
 }
--- src/main/java/com/hy/crm/mapper/AftersaleMapper.java
+++ src/main/java/com/hy/crm/mapper/AftersaleMapper.java
@@ -1,7 +1,16 @@
 package com.hy.crm.mapper;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.hy.crm.pojo.Aftersale;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.hy.crm.pojo.Contract;
+import com.hy.crm.pojo.vo.AftersaleExt;
+import com.hy.crm.utils.SqlUtils;
+import org.apache.ibatis.annotations.*;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 /**
  * <p>
@@ -13,4 +22,12 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  */
 public interface AftersaleMapper extends BaseMapper<Aftersale> {
 
+    @Results({
+        @Result(column = "conid",property = "contract",one = @One(select = "queryById"))
+    })
+    @SelectProvider(type= SqlUtils.class,method="select1")
+    public IPage<AftersaleExt> queryAll(Page<AftersaleExt> page, @Param("val") String val, @Param("keyWord") String keyWord);
+
+    @Select("select * from contract where conid=#{value}")
+    public Contract queryById();
 }
