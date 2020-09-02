994bc4f74706421d618f53c310939d59fcbc62af
yangsen <yangs251@qq.com>
2020-09-02 11:27:37 +0800

    杨森提交分之跟单服务

--- src/main/java/com/hy/crm/controller/DocumentaryController.java
+++ src/main/java/com/hy/crm/controller/DocumentaryController.java
@@ -4,10 +4,14 @@ package com.hy.crm.controller;
 import com.hy.crm.pojo.Documentary;
 import com.hy.crm.service.IDocumentaryService;
 import com.hy.crm.service.impl.DocumentaryServiceImpl;
+import com.hy.crm.utils.MsgUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
 
 /**
  * <p>
@@ -28,5 +32,16 @@ public class DocumentaryController {
         System.out.println(documentary.toString());
         iDocumentaryService.save(documentary);
     }
-
+    /*跟单查询*/
+    @RequestMapping("/selectdoc.do")
+    @ResponseBody
+    public MsgUtils selectdocumentary(Integer page, Integer limit, Documentary documentary){
+        ArrayList<Documentary> doclist =iDocumentaryService.selectdoc(page,limit,documentary);
+        MsgUtils msg=new MsgUtils();
+        msg.setCode("0");
+        msg.setMsg("doc");
+        msg.setCount(doclist.size());
+        msg.setData(doclist);
+        return msg;
+    }
 }
